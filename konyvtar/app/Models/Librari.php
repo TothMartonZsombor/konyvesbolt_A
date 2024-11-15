ab4c9bf3130642364e36c06db179bdc5c07c6e85
TothMartonZsombor <77171552+TothMartonZsombor@users.noreply.github.com>
2024-11-15 09:02:40 +0100

    09:02

--- konyvtar/app/Models/Librari.php
+++ konyvtar/app/Models/Librari.php
@@ -3,8 +3,16 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Librari extends Model
 {
-    
+    use HasFactory, SoftDeletes;
+
+    protected $fillable = ['subject_name'];
+
+    public function question(){
+        return $this->hasMany(Librari::class);
+    }
 }
